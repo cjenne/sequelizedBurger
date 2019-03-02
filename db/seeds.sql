c6b9d14997c3445e2ee6b4c4488475f1aa56eca1
Duane Dibbley <44147755+cjenne@users.noreply.github.com>
2019-03-02 09:56:01 -0700

    committing copied files from burger to sequelize

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Double Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Bacon Avacado Burger');
+INSERT INTO burgers (name, devoured) VALUES ('Turkey Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Chicken Burger ', true);
+INSERT INTO burgers (name, devoured) VALUES ('Double bacon and cheese Burger', true);
+INSERT INTO burgers (name) VALUES ('Tofu Burger');
