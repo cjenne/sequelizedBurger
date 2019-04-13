049d4a15b345b7d0e6ca611ac7374df476400dc9
Duane Dibbley <44147755+cjenne@users.noreply.github.com>
2019-04-13 12:23:20 -0700

    dsgfhsdfs yep

--- config/config.js
+++ config/config.js
@@ -1,8 +1,15 @@
-module exports = {
+module.exports = {
   "development": {
+    "username": process.env.MYSQL_USER,
+    "password": process.env.MYSQL_KEY,
+    "database": process.env.MYSQL_DBNAME,
+    "host": process.env.MYSQL_HOST,
+    "dialect": "mysql"
+  },
+  "test": {
     "username": "root",
-    "password": "",
-    "database": "burger_db",
+    "password": null,
+    "database": "database_test",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
