b8d45bba09eab98a758c9b845bbeccd9d450d14e
Duane Dibbley <44147755+cjenne@users.noreply.github.com>
2019-04-12 18:45:45 -0700

    updated env

--- config/config.js
+++ config/config.js
@@ -1,9 +1,9 @@
-{
+module.exports = {
   "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
+    "username": process.env.MYSQL_USER,
+    "password": process.env.MYSQL_KEY,
+    "database": process.env.MYSQL_DBNAME,
+    "host": process.env.MYSQL_HOST,
     "dialect": "mysql"
   },
   "test": {
@@ -14,7 +14,7 @@
     "dialect": "mysql"
   },
   "production": {
-    "use_env_variable": "JawsDB",
+    "use_env_variable": "JAWSDB_URL",
     "dialect": "mysql"
   }
 }
--- config/connection.js
+++ config/connection.js
@@ -1,38 +1,38 @@
-// Set up MySQL connection.
-var mysql = require("mysql");
-var connection;
+// // Set up MySQL connection.
+// var mysql = require("mysql");
+// var connection;
 
-if (process.env.JawsDB) {
-  // console.log("jawsdb connect");
-  connection = mysql.createConnection(process.env.JawsDB);
-} else {
-  // console.log("is local sql connect running");
-  connection = mysql.createConnection({
-  host: "localhost",
-  // port: 3306,
-  user: "root",
-  password: "Monk3yH8sPa$$word$",
-  database: "burger_db"
-  })
-}
-
-// var connection = mysql.createConnection({
+// if (process.env.JAWSDB_URL) {
+//   // console.log("jawsdb connect");
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+//   // console.log("is local sql connect running");
+//   connection = mysql.createConnection({
 //   host: "localhost",
-//   port: 3306,
+//   // port: 3306,
 //   user: "root",
-//   password: "removed password",
+//   password: "_remo",
 //   database: "burger_db"
-// });
+//   })
+// }
 
-// // Make connection.
-// connection.connect(function(err) {
-//   if (err) {
-//     console.error("error connecting: " + err.stack);
-//     return;
-//   }
-//   console.log("connected as id " + connection.threadId);
-// });
+// // var connection = mysql.createConnection({
+// //   host: "localhost",
+// //   port: 3306,
+// //   user: "root",
+// //   password: "removed password",
+// //   database: "burger_db"
+// // });
 
-// Export connection for our ORM to use.
+// // // Make connection.
+// // connection.connect(function(err) {
+// //   if (err) {
+// //     console.error("error connecting: " + err.stack);
+// //     return;
+// //   }
+// //   console.log("connected as id " + connection.threadId);
+// // });
+
+// // Export connection for our ORM to use.
 // connection.connect();
-module.exports = connection;
+// module.exports = connection;
