d6be1e059e4dc84518c09c00d4b553b7e38d7f97
Duane Dibbley <44147755+cjenne@users.noreply.github.com>
2019-04-12 23:52:05 -0700

    removing more files

--- db/seeds.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO burgers (name) VALUES ('Double Cheeseburger');
-INSERT INTO burgers (name) VALUES ('Bacon Avacado Burger');
-INSERT INTO burgers (name, devoured) VALUES ('Turkey Burger', true);
-INSERT INTO burgers (name, devoured) VALUES ('Chicken Burger ', true);
-INSERT INTO burgers (name, devoured) VALUES ('Double bacon and cheese Burger', true);
-INSERT INTO burgers (name) VALUES ('Tofu Burger');
